5c3ef7ddaff11a3e55037a48725d96b1511b387e
HyeYeonLee0112 <qquqqu26@naver.com>
2026-06-09 23:03:29 +0900

    feat: add auth register and login

--- back/app/main.py
+++ back/app/main.py
@@ -1,8 +1,13 @@
 #FastAPI 앱 시작점
-
 from fastapi import FastAPI
+from app import models
+from app.database import engine
+from app.routers import auth
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(auth.router)
 
 @app.get("/")
 async def root():
